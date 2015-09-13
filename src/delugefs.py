d1b1fe3b0958af602071c925c4bfb8af1a15213c
johnko <git@johnko.ca>
2015-09-13 16:00:46 -0400

    debug gitlog

--- src/delugefs.py
+++ src/delugefs.py
@@ -564,8 +564,9 @@ class DelugeFS(LoggingMixIn, Operations):
         self.repo.log('-1', '--no-color', '--pretty=tformat:%h', _out=self.__get_git_log_callback)
 
     def __get_git_log_callback(self, line):
-        print 'line',line.strip().split(None)[1]
-        self.httpd.api['gitlog'] = line.strip().split(None)[1]
+        abbrevcommit = line.strip().split(None)[1]
+        print 'abbrevcommit',abbrevcommit
+        self.httpd.api['gitlog'] = abbrevcommit
 
     def __keep_pushing(self):
         while True:
