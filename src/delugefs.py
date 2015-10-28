ce89093c98b0be368243feff62fae16b426d4ace
johnko <git@johnko.ca>
2015-10-28 04:06:05 -0400

    remove py-sh import

--- src/delugefs.py
+++ src/delugefs.py
@@ -24,7 +24,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import os, errno, sys, threading, collections, uuid, shutil, traceback, random, select, time, socket, multiprocessing, stat, datetime, statvfs, math, BaseHTTPServer, SocketServer, hashlib
 from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
 import libtorrent
-import sh # try to code it so we can remove 'import sh' for windows
 import pybonjour
 from webapi import WebUIServer, WebUIHandler
 
